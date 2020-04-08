cc2fdf4964acf9f130758f32f9d9b0c1054d49a5
kasidevi <kasidevi@gmail.com>
2020-04-08 16:04:42 +0530

    merge cof

--- src/components/EmojiGame/EmojiCard.js
+++ src/components/EmojiGame/EmojiCard.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { EachEmoji, DivTagWithFlexcol, PTag } from '../EmojiGameCss/EmojiGameCss.js';
 class EmojiCard extends React.Component {
-    render() {
+   render() {
 
         const { emoji, onEmojiClick } = this.props;
         return (<DivTagWithFlexcol onClick={onEmojiClick}>
-        <EachEmoji src={emoji.image} />
-        <PTag>{emoji.name}</PTag>
+            <EachEmoji src={emoji.image} />
+            <PTag>{emoji.name}</PTag>
         </DivTagWithFlexcol>);
     }
 }
