cec240d1f2a8772c30e93f66be8e44fd94060d8d
desrikomati999 <devisrikomati1221@gmail.com>
2020-04-08 11:49:33 +0000

    changes from lapy

--- src/components/EmojiGame/EmojiCard.js
+++ src/components/EmojiGame/EmojiCard.js
@@ -2,13 +2,11 @@ import React from 'react';
 import { EachEmoji, DivForEmojiCard, EmojiName } from '../EmojiGameCss/EmojiGameCss.js';
 class EmojiCard extends React.Component {
     render() {
-
-        const { emoji, onEmojiClick } = this.props;
-        return (<DivTagWithFlexcol onClick={onEmojiClick}>
-            <EachEmoji src={emoji.image} />
-            <PTag>{emoji.name}</PTag>
-        </DivTagWithFlexcol>);
-
+        const { emoji, onEmojiClick, selectedTheme } = this.props;
+        return (<DivForEmojiCard onClick={onEmojiClick} selectedTheme={selectedTheme}>
+        <EachEmoji src={emoji.image} />
+        <EmojiName>{emoji.name}</EmojiName>
+        </DivForEmojiCard>);
     }
 }
 export default EmojiCard;
